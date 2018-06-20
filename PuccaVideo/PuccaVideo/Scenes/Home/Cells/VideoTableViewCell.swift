156a0bd2e9b6c8cd13a3a32dcbf5fbf089314d98
Isabela Karen Louli <[isahkaren16@gmail.com]]>
2018-06-20 18:56:24 +0200

    Video play screen added

---
+++ PuccaVideo/PuccaVideo/Scenes/Home/Cells/VideoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  VideoTableViewCell.swift
+//  PuccaVideo
+//
+//  Created by Diego Louli on 20.06.18.
+//  Copyright © 2018 Isabela Karen Louli. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var authorProfileImage: UIImageView!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+}
---
+++ PuccaVideo/PuccaVideo/Scenes/Home/ViewControllers/VideoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  VideoViewController.swift
+//  PuccaVideo
+//
+//  Created by Diego Louli on 20.06.18.
+//  Copyright © 2018 Isabela Karen Louli. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+    
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
